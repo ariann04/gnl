08018589b4bb6050227c40e1fda873531f6de37e
ariann04 <142428851+ariann04@users.noreply.github.com>
2024-06-12 20:31:28 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -11,10 +11,10 @@
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 42
-#endif
+#  define GET_NEXT_LINE_H
+#  ifndef BUFFER_SIZE
+#    define BUFFER_SIZE 42
+#  endif
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
